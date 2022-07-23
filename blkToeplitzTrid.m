570c84e950449428c73d26cb7add50521af3f089
AlexandrosNatsollari <81075113+AlexandrosNatsollari@users.noreply.github.com>
2022-07-23 09:38:32 +0300

    Update blkToeplitzTrid.m

--- blkToeplitzTrid.m
+++ blkToeplitzTrid.m
@@ -1,6 +1,5 @@
 function S=blkToeplitzTrid(n,B,A,C)
-% Author : ALEXANDROS NATSOLLARI , AM:1057769 , Date : 21/11/2021
-
+% Author : ALEXANDROS NATSOLLARI 
 S = sparse(kron(eye(n),A) + kron(diag(ones(n-1,1), -1),B)+ kron(diag(ones(n-1,1), 1),C));
 
 %full(S)
